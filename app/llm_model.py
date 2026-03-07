676bbbc190ba60a0e48237db6196d0dd906eed54
Nisarg-Panchal876 <nisargjjp@gmail.com>
2026-03-07 11:29:03 +0530

    added env file

--- app/llm_model.py
+++ app/llm_model.py
@@ -1,8 +1,9 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 import os
+from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage,AIMessage,ToolMessage
 
-os.environ["GOOGLE_API_KEY"] = "AIzaSyApkATVF4lSyCcom-aFzmxiuCzgcFtm6bQ"
+load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",  
